264be54ee06023c300708a2695b2d1e15da69550
dekanoidze <lukadekanoidze173@gmail.com>
2026-04-16 19:38:39 +0400

    Initial commit

---
+++ Desktop/for java web project/ByteSized/src/main/java/ge/tsu/ByteSized/ByteSizedApplication.java
@@ -0,0 +1,13 @@
+package ge.tsu.ByteSized;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByteSizedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ByteSizedApplication.class, args);
+	}
+
+}
---
+++ Desktop/for java web project/ByteSized/src/test/java/ge/tsu/ByteSized/ByteSizedApplicationTests.java
@@ -0,0 +1,13 @@
+package ge.tsu.ByteSized;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByteSizedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
